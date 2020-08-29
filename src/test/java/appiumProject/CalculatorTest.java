2c62dc9d0c68f70163b2d98169a24f349aa5bcf7
juhi16 <pjuhi5300@gmail.com>
2020-08-29 17:16:20 -0400

    Appium commit

---
+++ src/test/java/appiumProject/CalculatorTest.java
@@ -0,0 +1,63 @@
+package appiumProject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class CalculatorTest {
+	
+	static AppiumDriver<MobileElement> driver;
+
+	public static void main(String[] args) {
+		
+		try {
+			openCalculator();
+		} catch (Exception e) {
+			
+			System.out.println(e.getCause());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public static void openCalculator() throws Exception  {
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		cap.setCapability( "deviceName", "Juhiemulator");
+		cap.setCapability("udid", "emulator-5554");
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("platformVersion", "9");
+		
+		cap.setCapability("appPackage", "com.android.calculator2");
+		cap.setCapability("appActivity", "com.android.calculator2.Calculator");
+		
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		driver = new AppiumDriver<MobileElement>(url,cap);
+		
+		System.out.println("Application Started......");
+
+		MobileElement two = driver.findElement(By.id("com.android.calculator2:id/digit_2"));
+		MobileElement plus = driver.findElement(By.id("com.android.calculator2:id/op_add"));
+		MobileElement three = driver.findElement(By.id("com.android.calculator2:id/digit_3"));
+		MobileElement equals = driver.findElement(By.id("com.android.calculator2:id/eq"));
+		
+		MobileElement result = driver.findElement(By.id("com.android.calculator2:id/result"));
+		
+		two.click();
+		plus.click();
+		three.click();
+		equals.click();
+		
+		String res = result.getText();
+		
+		System.out.println("\nResult is : " +res);
+		System.out.println("Completed......");
+	}
+
+}
